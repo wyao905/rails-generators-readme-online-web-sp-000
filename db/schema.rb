80b91f67eaf92f8012173f1ce7880a7d128b08bd
Warren Yao <warrenyca905@gmail.com>
2019-11-21 00:18:19 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121000149) do
+ActiveRecord::Schema.define(version: 20191121001651) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 20191121000149) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
